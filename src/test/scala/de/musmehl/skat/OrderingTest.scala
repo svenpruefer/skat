316ba9dc5ceae980823f02eeaffd49aed950ce27
Sven Prüfer <pruefer.sven@gmail.com>
2018-04-17 23:16:44 +0200

    Add simple ordering test

--- src/test/scala/de/musmehl/skat/OrderingTest.scala
+++ src/test/scala/de/musmehl/skat/OrderingTest.scala
@@ -1,5 +1,15 @@
 package de.musmehl.skat
 
-class OrderingTest {
+import org.scalatest.FunSuite
+
+class OrderingTest extends FunSuite {
+
+    test("Simple Ordering Test of Colors") {
+        import de.musmehl.skat.colorOrdering
+
+        val input = List[Color](Grün, Eichel, Rot, Schell)
+
+        assert(input.sorted == List[Color](Schell, Rot, Grün, Eichel))
+    }
 
 }
