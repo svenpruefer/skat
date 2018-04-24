5465bb8bc49f4036a1b45fa248c1a184188604ee
Sven Prüfer <pruefer.sven@gmail.com>
2018-04-24 23:42:38 +0200

    Continue implementing game

--- src/main/scala/de/musmehl/skat/Match.scala
+++ src/main/scala/de/musmehl/skat/Match.scala
@@ -1,5 +1,7 @@
 package de.musmehl.skat
 
+import scala.collection.immutable.Queue
+
 trait MatchBase {
 
     def id: String
@@ -29,7 +31,7 @@ case class Match(id: String, players: List[Player]) extends MatchBase {
             (players(2), Set.empty[Card])
         )
 
-        val (dealtCards, skat) = shuffledCards.foldLeft((noDealtCards, Set.empty[Card])) ((agg, el) => {
+        val (dealtCards, skat) = shuffledCards.foldLeft((noDealtCards, Set.empty[Card]))((agg, el) => {
             shuffledCards.indexOf(el) match {
                 case i: Int if Set(0, 1, 2, 11, 12, 13, 14, 23, 24, 25).contains(i) =>
                     (agg._1.updated(players.head, agg._1(players.head) + el), agg._2)
@@ -56,13 +58,46 @@ case class Match(id: String, players: List[Player]) extends MatchBase {
 
     override def reizen(matchState: MatchState): MatchState = ???
 
-    override def playRound(matchState: MatchState): MatchState = ???
+    override def playRound(matchState: MatchState): MatchState = {
+        require(matchState.game.isDefined, "A round can only be played if there is a game chosen")
+        require(matchState.singlePlayer.isDefined, "A round can only be played if there is a single player chosen")
+        require(matchState.order.length == 3, "A round can only be played if there are three players involved")
+        require(matchState.skat.isEmpty, "A round can only be played if the Skat was already given to the single player")
+
+        val playedCards = Map.empty[Player, Card]
+
+        val (firstPlayer, remainingPlayers) = matchState.order.dequeue
+        val afterFirstPlayer = playedCards +
+            (firstPlayer -> firstPlayer.plays(matchState.playerStates(firstPlayer), matchState.game.get, Set.empty[Card]))
+
+        val (secondPlayer, lastPlayers) = remainingPlayers.dequeue
+        val afterSecondPlayer = afterFirstPlayer +
+            (secondPlayer -> secondPlayer.plays(matchState.playerStates(secondPlayer), matchState.game.get, afterFirstPlayer.values.toSet))
+
+        val thirdPlayer = lastPlayers.last
+        val totalStich = afterSecondPlayer +
+            (thirdPlayer -> thirdPlayer.plays(matchState.playerStates(thirdPlayer), matchState.game.get, afterSecondPlayer.values.toSet))
+
+        val winner = matchState.game.get.determineWinnerOfStich(totalStich)
+
+        MatchState(
+            Map[Player, PlayerState](
+                (players.head, matchState.playerStates(players.head).playCard(totalStich(players.head))),
+                (players(1), matchState.playerStates(players(1)).playCard(totalStich(players(1)))),
+                (players(2), matchState.playerStates(players(2)).playCard(totalStich(players(2))))
+            ),
+            ,
+            Set.empty[Card],
+            matchState.game,
+            matchState.singlePlayer
+        )
+    }
 
     override def playFullGame: Map[Player, Int] = ???
 }
 
 case class MatchState(playerStates: Map[Player, PlayerState],
-                      order: List[Player],
+                      order: Queue[Player],
                       skat: Set[Card],
                       game: Option[Game],
                       singlePlayer: Option[Player])
--- src/main/scala/de/musmehl/skat/PlayerBase.scala
+++ src/main/scala/de/musmehl/skat/PlayerBase.scala
@@ -9,9 +9,17 @@ trait PlayerBase {
     def sagt(reizValue: Option[Int]): Option[Int]
 
     def hört(reizValue: Int): Boolean
+
+    def plays(playerState: PlayerState, game: Game, onTable: Set[Card]): Card
 }
 
 case class Player(name: String,
                   totalPoints: Int) extends PlayerBase
 
-case class PlayerState(cards: Set[Card], stiche: Set[Stich], weg: Boolean)
+case class PlayerState(cards: Set[Card], stiche: Set[Stich], weg: Boolean) {
+
+    def playCard(card: Card): PlayerState = {
+        require(cards.contains(card), "In order to play a card, this card must be in possession of the player")
+        copy(cards = this.cards - card)
+    }
+}
