535e394f80b17e81f153082eaf82e121ac655647
Sven Prüfer <pruefer.sven@gmail.com>
2018-05-01 22:27:38 +0200

    small refactoring

--- src/main/scala/de/musmehl/skat/Match.scala
+++ src/main/scala/de/musmehl/skat/Match.scala
@@ -4,100 +4,100 @@ import scala.collection.immutable.Queue
 
 trait MatchBase {
 
-    def id: String
+  def id: String
 
-    def players: List[Player]
+  def players: List[Player]
 
-    def dealCards: MatchState
+  def dealCards: MatchState
 
-    def reizen(matchState: MatchState): MatchState
+  def reizen(matchState: MatchState): MatchState
 
-    def playRound(matchState: MatchState): MatchState
+  def playRound(matchState: MatchState): MatchState
 
-    def playFullGame: Map[Player, Int]
+  def playFullGame: Map[Player, Int]
 
 }
 
 case class Match(id: String, players: List[Player]) extends MatchBase {
 
-    require(players.length == 3, "Skat can only be played by exactly three players")
+  require(players.length == 3, "Skat can only be played by exactly three players")
 
-    override def dealCards: MatchState = {
-        val shuffledCards = scala.util.Random.shuffle(Card.allCards)
+  override def dealCards: MatchState = {
+    val shuffledCards = scala.util.Random.shuffle(Card.allCards)
 
-        val noDealtCards = Map[Player, Set[Card]](
-            (players.head, Set.empty[Card]),
-            (players(1), Set.empty[Card]),
-            (players(2), Set.empty[Card])
-        )
+    val noDealtCards = Map[Player, Set[Card]](
+      (players.head, Set.empty[Card]),
+      (players(1), Set.empty[Card]),
+      (players(2), Set.empty[Card]))
 
-        val (dealtCards, skat) = shuffledCards.foldLeft((noDealtCards, Set.empty[Card]))((agg, el) => {
-            shuffledCards.indexOf(el) match {
-                case i: Int if Set(0, 1, 2, 11, 12, 13, 14, 23, 24, 25).contains(i) =>
-                    (agg._1.updated(players.head, agg._1(players.head) + el), agg._2)
-                case i: Int if Set(3, 4, 5, 15, 16, 17, 18, 26, 27, 28).contains(i) =>
-                    (agg._1.updated(players(1), agg._1(players(1)) + el), agg._2)
-                case i: Int if Set(6, 7, 8, 19, 20, 21, 22, 29, 30, 31).contains(i) =>
-                    (agg._1.updated(players(2), agg._1(players(2)) + el), agg._2)
-                case i: Int if Set(9, 10).contains(i) => (agg._1, agg._2 + el)
-            }
-        })
+    val (dealtCards, skat) = shuffledCards.foldLeft((noDealtCards, Set.empty[Card]))((agg, el) => {
+      shuffledCards.indexOf(el) match {
+        case i: Int if Set(0, 1, 2, 11, 12, 13, 14, 23, 24, 25).contains(i) =>
+          (agg._1.updated(players.head, agg._1(players.head) + el), agg._2)
+        case i: Int if Set(3, 4, 5, 15, 16, 17, 18, 26, 27, 28).contains(i) =>
+          (agg._1.updated(players(1), agg._1(players(1)) + el), agg._2)
+        case i: Int if Set(6, 7, 8, 19, 20, 21, 22, 29, 30, 31).contains(i) =>
+          (agg._1.updated(players(2), agg._1(players(2)) + el), agg._2)
+        case i: Int if Set(9, 10).contains(i) => (agg._1, agg._2 + el)
+      }
+    })
 
-        MatchState(
-            Map[Player, PlayerState](
-                (players.head, PlayerState(dealtCards(players.head), Set.empty[Stich], weg = false)),
-                (players(1), PlayerState(dealtCards(players(1)), Set.empty[Stich], weg = false)),
-                (players(2), PlayerState(dealtCards(players(2)), Set.empty[Stich], weg = false))
-            ),
-            players,
-            skat,
-            None,
-            None
-        )
-    }
+    MatchState(
+      Map[Player, PlayerState](
+        (players.head, PlayerState(dealtCards(players.head), Set.empty[Stich], weg = false)),
+        (players(1), PlayerState(dealtCards(players(1)), Set.empty[Stich], weg = false)),
+        (players(2), PlayerState(dealtCards(players(2)), Set.empty[Stich], weg = false))),
+      Queue(players: _*),
+      skat,
+      None,
+      None)
+  }
 
-    override def reizen(matchState: MatchState): MatchState = ???
+  override def reizen(matchState: MatchState): MatchState = ???
 
-    override def playRound(matchState: MatchState): MatchState = {
-        require(matchState.game.isDefined, "A round can only be played if there is a game chosen")
-        require(matchState.singlePlayer.isDefined, "A round can only be played if there is a single player chosen")
-        require(matchState.order.length == 3, "A round can only be played if there are three players involved")
-        require(matchState.skat.isEmpty, "A round can only be played if the Skat was already given to the single player")
+  override def playRound(matchState: MatchState): MatchState = {
+    require(matchState.game.isDefined, "A round can only be played if there is a game chosen")
+    require(matchState.singlePlayer.isDefined, "A round can only be played if there is a single player chosen")
+    require(matchState.order.length == 3, "A round can only be played if there are three players involved")
+    require(matchState.skat.isEmpty, "A round can only be played if the Skat was already given to the single player")
 
-        val playedCards = Map.empty[Player, Card]
+    val playedCards = Map.empty[Player, Card]
 
-        val (firstPlayer, remainingPlayers) = matchState.order.dequeue
-        val afterFirstPlayer = playedCards +
-            (firstPlayer -> firstPlayer.plays(matchState.playerStates(firstPlayer), matchState.game.get, Set.empty[Card]))
+    val (firstPlayer, remainingPlayers) = matchState.order.dequeue
+    val afterFirstPlayer = playedCards +
+      (firstPlayer -> firstPlayer.plays(matchState.playerStates(firstPlayer), matchState.game.get, Set.empty[Card]))
 
-        val (secondPlayer, lastPlayers) = remainingPlayers.dequeue
-        val afterSecondPlayer = afterFirstPlayer +
-            (secondPlayer -> secondPlayer.plays(matchState.playerStates(secondPlayer), matchState.game.get, afterFirstPlayer.values.toSet))
+    val (secondPlayer, lastPlayers) = remainingPlayers.dequeue
+    val afterSecondPlayer = afterFirstPlayer +
+      (secondPlayer -> secondPlayer.plays(matchState.playerStates(secondPlayer), matchState.game.get, afterFirstPlayer.values.toSet))
 
-        val thirdPlayer = lastPlayers.last
-        val totalStich = afterSecondPlayer +
-            (thirdPlayer -> thirdPlayer.plays(matchState.playerStates(thirdPlayer), matchState.game.get, afterSecondPlayer.values.toSet))
+    val thirdPlayer = lastPlayers.last
+    val totalStich = afterSecondPlayer +
+      (thirdPlayer -> thirdPlayer.plays(matchState.playerStates(thirdPlayer), matchState.game.get, afterSecondPlayer.values.toSet))
 
-        val winner = matchState.game.get.determineWinnerOfStich(totalStich)
+    val winner = matchState.game.get.determineWinnerOfStich(totalStich)
 
-        MatchState(
-            Map[Player, PlayerState](
-                (players.head, matchState.playerStates(players.head).playCard(totalStich(players.head))),
-                (players(1), matchState.playerStates(players(1)).playCard(totalStich(players(1)))),
-                (players(2), matchState.playerStates(players(2)).playCard(totalStich(players(2))))
-            ),
-            ,
-            Set.empty[Card],
-            matchState.game,
-            matchState.singlePlayer
-        )
-    }
+    val indexOfWinner = matchState.order.indexOf(winner)
+    val (firstPart, secondPart) = matchState.order.splitAt(indexOfWinner)
+    val newOrder = secondPart.enqueue(firstPart)
 
-    override def playFullGame: Map[Player, Int] = ???
+    MatchState(
+      Map[Player, PlayerState](
+        (players.head, matchState.playerStates(players.head).playCard(totalStich(players.head))),
+        (players(1), matchState.playerStates(players(1)).playCard(totalStich(players(1)))),
+        (players(2), matchState.playerStates(players(2)).playCard(totalStich(players(2))))),
+      newOrder,
+      Set.empty[Card],
+      matchState.game,
+      matchState.singlePlayer)
+  }
+
+  override def playFullGame: Map[Player, Int] = ???
 }
 
-case class MatchState(playerStates: Map[Player, PlayerState],
-                      order: Queue[Player],
-                      skat: Set[Card],
-                      game: Option[Game],
-                      singlePlayer: Option[Player])
+case class MatchState(
+  playerStates: Map[Player, PlayerState],
+  order: Queue[Player],
+  skat: Set[Card],
+  game: Option[Game],
+  singlePlayer: Option[Player])
--- src/main/scala/de/musmehl/skat/PlayerBase.scala
+++ src/main/scala/de/musmehl/skat/PlayerBase.scala
@@ -2,24 +2,31 @@ package de.musmehl.skat
 
 trait PlayerBase {
 
-    def name: String
+  def name: String
 
-    def totalPoints: Int
+  def totalPoints: Int
 
-    def sagt(reizValue: Option[Int]): Option[Int]
+  def sagt(reizValue: Option[Int]): Option[Int]
 
-    def hört(reizValue: Int): Boolean
+  def hört(reizValue: Int): Boolean
 
-    def plays(playerState: PlayerState, game: Game, onTable: Set[Card]): Card
+  def plays(playerState: PlayerState, game: Game, onTable: Set[Card]): Card
 }
 
-case class Player(name: String,
-                  totalPoints: Int) extends PlayerBase
+case class Player(
+  name: String,
+  totalPoints: Int) extends PlayerBase {
+  override def sagt(reizValue: Option[Int]): Option[Int] = ???
+
+  override def hört(reizValue: Int): Boolean = ???
+
+  override def plays(playerState: PlayerState, game: Game, onTable: Set[Card]): Card = ???
+}
 
 case class PlayerState(cards: Set[Card], stiche: Set[Stich], weg: Boolean) {
 
-    def playCard(card: Card): PlayerState = {
-        require(cards.contains(card), "In order to play a card, this card must be in possession of the player")
-        copy(cards = this.cards - card)
-    }
+  def playCard(card: Card): PlayerState = {
+    require(cards.contains(card), "In order to play a card, this card must be in possession of the player")
+    copy(cards = this.cards - card)
+  }
 }
